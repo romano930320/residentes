ce5e7faef69a0267068fe3cf4fd41efdfc18fb0a
romano930320 <robertomacedonorato@gmail.com>
2026-04-28 00:53:54 -0600

    Add SSL configuration to database connection

--- api/auth/login.js
+++ api/auth/login.js
@@ -7,6 +7,7 @@ const pool = new Pool({
     ssl: { rejectUnauthorized: false }
 });
 
+
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Método no permitido' });
